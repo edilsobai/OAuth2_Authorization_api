3b038838c8d8e6a05d74beed7073c40a5987d7ab
Edil Usobaiev <usobaiev1@gmail.com>
2023-10-03 17:26:10 +0600

    Resticting access to endpoints

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const https = require("https")
 const helmet = require("helmet") 
 const express = require("express")
 const passport = require("passport")
+const cookieSession = require("cookie-session")
 const { Strategy } = require("passport-google-oauth20")
 require("dotenv").config()
 
@@ -12,16 +13,17 @@ const app = express()
 
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+
 }
-
-
 const AUTH_OPTIONS = {
     clientID: config.CLIENT_ID,
     clientSecret: config.CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL: "https://localhost:5000/auth/google/callback",
 }
-console.log(config)
+
 function verifyCallback(accessToken, refreshToken, profile, done){
     console.log("Google profile", profile)
     done(null, profile)
@@ -29,11 +31,28 @@ function verifyCallback(accessToken, refreshToken, profile, done){
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    done(null, id)
+}) 
+
 app.use(helmet())
+
+app.use(cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+}))
+
 app.use(passport.initialize())
+app.use(passport.session())
 
 function checkLoggedIn(req,res,next){
-    const isLogged = true
+    console.log("Current user is: ", req.user )
+    const isLogged = req.isAuthenticated && req.user
     if(!isLogged){
         return res.status(401).json({
             error: "You must log in!"
@@ -51,11 +70,12 @@ app.get("/auth/google/callback",
     passport.authenticate("google", {
         failureRedirect: "/failure",
         successRedirect: "/",
-        session: false
+        session: true
      }),
-    (req, res) =>{
-        console.log("Google called us back")
-    })
+    (req, res) => {
+        
+    console.log("Google called us back")
+})
 
 app.get("/auth/logout", (req, res) => {})
 
